1bde18b26ed603127b5e44eb95c0f461fb05d107
SebastianLlanes <sebastianllanessaavedra@gmail.com>
2022-12-20 19:11:19 -0300

    inicio de proyecto

--- Proyecto Camino Argentina en Qatar22/code.js
+++ Proyecto Camino Argentina en Qatar22/code.js
@@ -1,8 +1,11 @@
 const btnSol = document.querySelector('.btn-sol')
+
+const jugadores = document.getElementById('section-segunda')
 // const flag = document.querySelector('.seccion-bienvenida')
 
  btnSol.addEventListener('click', () => {
  document.getElementById('section-hide').style.display = 'none';
+ jugadores.classList.remove('inactive')
     })
 
  
