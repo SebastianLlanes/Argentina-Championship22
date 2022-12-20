cc193f789779f2acbf723ceb816bd9feb292b6b3
SebastianLlanes <111186162+SebastianLlanes@users.noreply.github.com>
2022-12-20 18:02:14 -0300

    Add files via upload

---
+++ Proyecto Camino Argentina en Qatar22/code.js
@@ -0,0 +1,10 @@
+const btnSol = document.querySelector('.btn-sol')
+// const flag = document.querySelector('.seccion-bienvenida')
+
+ btnSol.addEventListener('click', () => {
+ document.getElementById('section-hide').style.display = 'none';
+    })
+
+ 
+    
+ 
