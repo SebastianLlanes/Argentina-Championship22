1d3acbc54dd696d86b3d4a38eb6f34bc4cebeae6
SebastianLlanes <sebastianllanessaavedra@gmail.com>
2022-12-21 23:35:47 -0300

    3er section

--- Proyecto Camino Argentina en Qatar22/code.js
+++ Proyecto Camino Argentina en Qatar22/code.js
@@ -1,11 +1,16 @@
 const btnSol = document.querySelector('.btn-sol')
 
 const jugadores = document.getElementById('section-segunda')
+
+const capitulos = document.getElementById('section-tercera')
 // const flag = document.querySelector('.seccion-bienvenida')
 
  btnSol.addEventListener('click', () => {
  document.getElementById('section-hide').style.display = 'none';
- jugadores.classList.remove('inactive')
+
+ jugadores.classList.remove('inactive');
+
+ capitulos.classList.remove('inactive');
     })
 
  
